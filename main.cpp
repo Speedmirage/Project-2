a56f05d950c1a5abb7e1a8e5528edb8e112e4c5c
Addiel Almonte <Addiel.Almonte@jjay.cuny.edu>
2017-11-06 14:42:04 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,9 +11,41 @@
 #include "GameEntry.hpp"
 int main(int argc, const char * argv[]) {
     
+    StringLinkedList s;
+    string GameList;
+    int intList;
+    double doubleList;
+    int count;
+    string addGL;
+    
+    string Error = "Unable to enter this score.. List is full";
+    
+    cout<<"Please input the size of the GameEntry list"<<endl;
+    cin>>count;
     
     
+    for (int i = 1; i<(count+50); i++) { //Count+50 to be able to utilize the exception handling
+        
+        cout<<"Please input the score";
+        cin>>addGL;
+        s.addFront(addGL);
+        GameList = s.front();
+        //Exception handling below
+        try {
+            if ( i > count) //i-1 to make it run the exact number of times
+                throw GameEntryExept(Error);
+        } catch (GameEntryExept& me){
+            
+            cout<<"Unable to enter this score.. List is full"<<endl; //Exception is working
+        }
+        
+        
+        
+        
+    }
     
+    
+
     
     
     
