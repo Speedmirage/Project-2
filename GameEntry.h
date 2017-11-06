71714a896ab57f73bea8385818ca498b46338f08
Addiel Almonte <Addiel.Almonte@jjay.cuny.edu>
2017-11-06 14:49:33 -0500

    Update GameEntry.h

--- GameEntry.h
+++ GameEntry.h
@@ -1,13 +1,41 @@
-#include <iostream>
+//
+//  GameEntry.hpp
+//  Assigment 2
+//
+//  Created by Addiel Almonte on 10/29/17.
+//  Copyright © 2017 Addiel Almonte. All rights reserved.
+//
 
+#ifndef GameEntry_hpp
+#define GameEntry_hpp
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
 using namespace std;
 
-class GameEntry {				// a game score entry
-  public:
-    GameEntry(const string& n="", int s=0);	// constructor
-    string getName() const;			// get player name
-    int getScore() const;			// get score
-  private:
-    string name;				// player's name
-    int score;					// player's score
-  };
+class GameEntry{
+    
+public:
+    GameEntry(const string& n="", int s=0);
+    std::string getName() const;
+    int getScore() const ;
+private:
+    std::string Name;
+    int Score;
+    
+};
+
+class GameEntryExept : public GameEntry //RuntimeExpt
+{
+
+private:
+    string errMSG;
+public:
+    GameEntryExept(const string& err):errMSG(err){}
+    std::string getError(){return errMSG;}
+    void setError(string);
+    
+};
+
+#endif /* GameEntry_hpp */
